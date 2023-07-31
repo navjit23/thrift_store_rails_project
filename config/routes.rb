dff7e0f901bbe72c7c1e0e2d1e487aa85607d2f2
navjit23 <66906714+navjit23@users.noreply.github.com>
2023-07-31 08:54:25 -0500

    implemented a login system

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :orders
   resources :products_ordereds
   resources :prices
@@ -11,5 +12,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#index"
+  get '/index', to: 'pages#index'
+  root 'pages#index'
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_21_151228) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_30_224427) do
   create_table "admins", force: :cascade do |t|
     t.string "name"
     t.string "password"
@@ -90,6 +90,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_21_151228) do
     t.string "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
