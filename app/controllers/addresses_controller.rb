2eaf97a20a554bf63f6e51b1960d386a585353ca
navjit23 <66906714+navjit23@users.noreply.github.com>
2023-08-10 12:04:41 -0500

    checckout works with logged in user

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,7 +3,7 @@ class AddressesController < InheritedResources::Base
   private
 
     def address_params
-      params.require(:address).permit(:address, :postal, :province, :user_id)
+      params.require(:postal).permit(:address, :postal, :province, :user_id)
     end
 
 end
